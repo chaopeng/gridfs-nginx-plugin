e9ffd123be80cc2d185dd26437f913c30173e57f
chaopeng <139025826@qq.com>
2014-11-25 14:37:46 -0500

    update install script

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ cd nginx-dev
 git clone https://github.com/nginx/nginx.git
 git clone https://github.com/chaopeng/gridfs-nginx-plugin.git
 
-cd gridfs-nginx-plugin.git
+cd gridfs-nginx-plugin
 ./build.sh
