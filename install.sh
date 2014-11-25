0cb8dbaf9360aead8659598c658d723b49d0e7d7
chaopeng <139025826@qq.com>
2014-11-25 14:23:32 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir nginx-dev
+cd nginx-dev
+
+git clone https://github.com/nginx/nginx.git
+git clone https://github.com/chaopeng/gridfs-nginx-plugin.git
+
+cd gridfs-nginx-plugin.git
+./build.sh
